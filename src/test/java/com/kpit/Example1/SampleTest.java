5b8b9cebb9bb31f5274454ac9085067293028c0a
Suleman Soudagar <sulemans@kpit.com>
2018-09-24 14:22:14 +0530

    second commit

---
+++ src/test/java/com/kpit/Example1/SampleTest.java
@@ -0,0 +1,14 @@
+package com.kpit.Example1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SampleTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
